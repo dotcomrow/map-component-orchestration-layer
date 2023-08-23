31e2ce17000f10c346e98e87cf62337319fffbc0
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-23 15:12:22 -0400

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- schema.py
+++ schema.py
@@ -4,14 +4,3 @@ from marshmallow_geojson import GeoJSONSchema
 class BaseSchema(Schema):
     location = fields.Nested(GeoJSONSchema)
     data = fields.Dict(required=True)
-    
-    def to_dict():
-        return {
-            'location': {
-                'type': 'Point',
-                'coordinates': [0, 0]
-            },
-            'data': {
-                'key':'value'
-            }
-        }
